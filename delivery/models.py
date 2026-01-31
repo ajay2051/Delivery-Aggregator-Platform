2fdc6eb0a6badb99362d9c6fd5452c6ddef99196
ajay🙋 <ajaythk.94@gmail.com>
2026-01-31 08:53:52 +0545

    added_related_names

--- delivery/models.py
+++ delivery/models.py
@@ -7,5 +7,8 @@ from utils.enums import DeliveryStatus
 class Delivery(BaseModel):
     status = models.CharField(choices=DeliveryStatus.choices(), default=DeliveryStatus.CREATED.value)
     delivery_date = models.DateField()
-    assigned_to = models.ForeignKey(AuthUser, on_delete=models.CASCADE)
-    created_by = models.ForeignKey(AuthUser, on_delete=models.CASCADE)
+    assigned_to = models.ForeignKey(AuthUser, on_delete=models.CASCADE, related_name='assigned_deliveries')
+    created_by = models.ForeignKey(AuthUser, on_delete=models.CASCADE, related_name='created_deliveries')
+
+    class Meta:
+        db_table = "deliveries"
